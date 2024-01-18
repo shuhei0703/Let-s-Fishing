11855b53d33f5ac9c4f904913e3eaebbecebfc87
shuhei0703 <148410402+shuhei0703@users.noreply.github.com>
2024-01-18 22:39:54 +0900

    Add files via upload

---
+++ Let's Fishing/maguro.js
@@ -0,0 +1,29 @@
+<!DOCTIPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="000.css"/>
+<title>齋藤秀平</title>
+</head>
+<body style="margin:0px;">
+<h1>
+    <td id=td5><div id="haikei"><h1>Let's Fishing</h1></div></td>
+  </h1>
+  <div id="nav">
+    <ul>
+</ul>
+
+</body>
+<li><a href="C:\Users\shuhe\OneDrive\ドキュメント\トップページ\課題7\トップページ.html">トップページへ</a>
+<script type="text/javascript">
+let score =100
+ 
+
+if(score===100){
+alert("おｋ")}
+<script>
+
+
+</body>
+ 
+</html>
